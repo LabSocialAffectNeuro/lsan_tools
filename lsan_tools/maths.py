944aba7fc007e5688d807a321c6adff31aae79a2
Shawn Rhoads <sa.rhoads93@gmail.com>
2019-11-15 22:34:03 -0500

    updated shuffle to include numpy arrays

--- lsan_tools/maths.py
+++ lsan_tools/maths.py
@@ -3,24 +3,28 @@ import numpy as np
 from sklearn.metrics import pairwise_distances
 from sklearn.preprocessing import StandardScaler
 
-def shuffle(df):
+def shuffle(df, type="pandas"):
     """
     Take a DataFrame where the columns are variables and the 
     observations are the rows (e.g., row indices are subject IDs),
     and randomly shuffles the row indices.
     """
-    
-    new_df = df.copy()
-    new_df['new_id'] = np.random.permutation(new_df.index)
+    if type == "pandas":
+	    perm_data = df.copy()
+	    perm_data['new_id'] = np.random.permutation(perm_data.index)
 
-    # get rid of added column
-    new_df.index = (new_df['new_id'])
-    
-    # get rid of index name
-    new_df.index.name = None
+	    # get rid of added column
+	    perm_data.index = (perm_data['new_id'])
+	    
+	    # get rid of index name
+	    perm_data.index.name = None
+	    
+	elif type == "numpy":
+		perm_data = np.random.permutation(df)
 
     # Now have subjects x variables DataFrame with subject IDs randomly shuffled.
-    return new_df
+    return perm_data
+
 
 
 def standardize(df, var_list):
