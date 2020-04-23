a616810cab8ed0bc1cfaf16c2c692c74e12698d7
Shawn Rhoads <sa.rhoads93@gmail.com>
2020-04-23 14:09:01 -0400

    added functions to compute pairwise difference, standardization

--- lsan_tools/math.py
+++ lsan_tools/math.py
@@ -3,6 +3,52 @@ import numpy as np
 from sklearn.metrics import pairwise_distances
 from sklearn.preprocessing import StandardScaler
 
+def normalize_btwn_0_1(list_obj):
+	"""
+	Takes a list and normalizes the values from 0 (smallest) to 1(largest)
+	"""
+	return (list_obj-min(list_obj))/(max(list_obj)-min(list_obj))
+
+def get_pairwise(behav_vct,type="absolute-dist",norm=True):
+    
+    """
+    Takes a vector of behavioral scores (one per subject) and returns 
+    the vectorized upper triangle of a similarity matrix constructed using:
+    	A) absolute distance
+    	B) average, or 
+    	C) one formulation of the "AnnaK" principle 
+    		(i.e., high-high pairs are most alike, low-low pairs are most dissimilar, and high-low pairs show intermediate similarity).
+    		(all high scorers are alike, all low scorers are low-scoring in their own way)
+    """    
+    # Get dims
+    n_subs = len(behav_vct)
+    
+    # Initialize output
+    mtx = np.zeros((n_subs,n_subs))
+    
+    if norm:
+    	behav_vct = normalize_btwn_0_1(behav_vct)
+
+    # Fill in matrix
+    for i in range(n_subs):
+        for j in range(n_subs):
+            if type == 'low-alike':
+                mtx[i,j] = max(behav_vct[i], behav_vct[j])
+                
+            elif type == 'high-alike':
+                mtx[i,j] = (1 - min(behav_vct[i], behav_vct[j])) #/n_subs
+                
+            elif type == 'average':
+                mtx[i,j] = (behav_vct[i]+behav_vct[j])/2
+                
+            elif type == 'absolute-dist':
+                mtx[i,j] = np.absolute(behav_vct[i]-behav_vct[j])
+                
+    # Compute upper triangle            
+    vct = mtx[np.triu_indices(mtx.shape[0], k=1)]
+    
+    return vct, mtx
+
 def shuffle(df, type="pandas"):
     """
     Take a DataFrame where the columns are variables and the 
@@ -25,46 +71,9 @@ def shuffle(df, type="pandas"):
     # Now have subjects x variables DataFrame with subject IDs randomly shuffled.
     return perm_data
 
-# def pairwise_vector(vec1, vec2, method="correlation", shuffle=False):
-#     """
-#     Takes a DateFrame and computes pairwise  
-#     distance between two column vectors
-
-#     Returns a 2D dissimilarity matrix as np.array
-#     """
-#     matrix = {}
-
-#     for sub_id1 in df.index:
-#         for sub_id2 in df.index:
-
-#             # need to get pairwise subjects
-#             sub1 = df.loc[sub_id]
-#             sub2 = 
-
-#         sub_pair = 
-
-#         matrix[sub_pair] = {}
-
-#         for variable in var_list:
-#             sub1_vector = df[variable].loc[sub1]
-#             sub2_vector = df[variable].loc[sub2]
-
-#             # if one of the subjects is missing data for this variable, replace with NaN
-#             if not sub1_vector or not sub2_vector:
-#                 dist = np.nan
-
-#             else:            
-#                 X = np.array([sub1_vector, sub2_vector])
-#                 dist = distance.pdist(X, metric=method)
-
-#             matrix[sub_pair][variable] = float(dist) 
-
-#     return matrix
-
-def standardize(df, var_list):
+def zscore_df(df, var_list):
     """ 
     Takes DateFrame and z-scores values within each of the columns
     """
     scaler = StandardScaler()
     return scaler.fit_transform(df[var_list])
-
